47c2d206f4f6523a9230887e10e219c8c6474228
Groovy04 <93429213+Groovy04@users.noreply.github.com>
2021-12-07 16:07:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://tansubaktiran:Avz9p9&9Dgsu_099@193.111.73.99/tansubaktiran"
 
 #Secret key
-app.config['SECRET_KEY'] = "MYSUPERKEY"
+app.config['SECRET_KEY'] = "$2b$12$esdzuq4Nr9kEpKvo1V2gx.ceNSwKTSG2lG4dwGrzFKi4R9HPwPn6m"
 #Initialize the adatabase
 db = SQLAlchemy(app)
 
@@ -798,4 +798,4 @@ def performance_graph():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
